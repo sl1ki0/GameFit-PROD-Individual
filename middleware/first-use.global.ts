a445a5c9b4c8fafd918aa09246df09b6cd1829e5
sl1ki0 <oiltanker9@gmail.com>
2025-02-13 20:33:32 +0300

    fix: middleware редиреткит на welcome при первом входе

--- middleware/first-use.global.ts
+++ middleware/first-use.global.ts
@@ -1,8 +1,12 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-    const firstVisit: String | null = localStorage.getItem('firstVisit')
+export default defineNuxtRouteMiddleware((to) => {
+      const firstVisit: String | null = localStorage.getItem("firstVisit");
 
-    if (!firstVisit) {
-        navigateTo('/welcome')
-    }
-  })
-  
+      if (!firstVisit && to.path === '/welcome') {
+        localStorage.setItem("firstVisit", "visited");
+        return
+      } else if (!firstVisit) {
+        localStorage.setItem("firstVisit", "visited");
+        return navigateTo("/welcome");
+      }
+
+  });
