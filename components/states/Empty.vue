f750094170b65a871545285598521742348b548c
sl1ki0 <oiltanker9@gmail.com>
2025-02-18 13:43:29 +0300

    фикс цвета (было плохо видно в темной теме)

--- components/states/Empty.vue
+++ components/states/Empty.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="text-center p-8">
-      <i class="pi pi-inbox text-6xl text-gray-400 mb-4" />
-      <p class="text-xl text-gray-600">
+      <i class="pi pi-inbox text-6xl mb-4" />
+      <p class="text-xl">
         <slot>Здесь пока ничего нет</slot>
       </p>
     </div>
