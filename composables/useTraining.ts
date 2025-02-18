c5538bf621d6e5ac70c2e79d39eaddf41c7633f9
sl1ki0 <oiltanker9@gmail.com>
2025-02-18 11:27:56 +0300

    composable для загрузки тренировок

---
+++ composables/useTraining.ts
@@ -0,0 +1,30 @@
+import { ref, onMounted } from 'vue';
+import trainingDataStorage from '~/storage/trainingData';
+import type Training from '~/types/trainings/TrainingType';
+
+
+export function useTrainings() {
+  const trainings = ref<Training[]>([]);
+  const isLoading = ref<boolean>(false);
+
+
+  const loadTrainings = async () => {
+    try {
+      isLoading.value = true;
+      const keys = await trainingDataStorage.keys();
+      const data = await Promise.all(
+        keys.map(async (key) => await trainingDataStorage.getItem(key) as Training)
+      );
+      trainings.value = data.filter(Boolean);
+    } catch (error) {
+      console.error('Ошибка загрузки данных:', error);
+    } finally {
+      isLoading.value = false;
+    }
+  };
+
+
+  onMounted(loadTrainings);
+
+  return { trainings, isLoading, loadTrainings };
+}
