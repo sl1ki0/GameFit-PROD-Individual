bd891abe8e0d2977a47b9f6348d310150e3e83a1
sl1ki0 <oiltanker9@gmail.com>
2025-02-19 17:31:14 +0300

    небольшое UI исправление тегов на странице со списком тренировок (теги выголядят аккуратно, не выезжают за границы)

--- components/TrainingCard.vue
+++ components/TrainingCard.vue
@@ -1,42 +1,45 @@
 <template>
-<Card class="overflow-hidden w-full lg:w-4/5">
-    <template #title>{{training.name}}</template>
-    <template #subtitle>{{training.muscleGroup}}</template>
+  <Card class="overflow-hidden w-full lg:w-4/5">
+    <template #title>{{ training.name }}</template>
+    <template #subtitle>{{ training.muscleGroup }}</template>
     <template #content>
       <div class="flex flex-col gap-4">
         <p class="m-0">
           {{ training.description }}
         </p>
-        <div class="flex w-full gap-2">
-          <Tag v-for="item in training.equipment" class="w-1/3 overflow-hidden" severity="secondary" rounded>{{item}}</Tag>
+        <div class="flex flex-wrap gap-2">
+          <Tag v-for="item in training.equipment" severity="secondary" :key="item"
+            class="inline-flex items-center justify-center px-2 py-1 text-sm rounded-lg min-w-fit max-w-[150px] truncate">
+            {{ item }}
+          </Tag>
         </div>
       </div>
     </template>
     <template #footer>
-        <div class="flex gap-4 mt-1">
-            <Button label="Удалить" severity="danger" outlined class="w-full" />
-            <Button label="Начать" class="w-full" />
-        </div>
+      <div class="flex gap-4 mt-1">
+        <Button label="Удалить" severity="danger" outlined class="w-full" />
+        <Button label="Начать" class="w-full" />
+      </div>
     </template>
-</Card>
+  </Card>
 </template>
-  
-  <script setup lang="ts">
-  import type Training from '~/types/trainings/TrainingType';
 
-  const props = defineProps<{
-    training: Training;
-  }>();
-  </script>
+<script setup lang="ts">
+import type Training from '~/types/trainings/TrainingType';
+
+const props = defineProps<{
+  training: Training;
+}>();
+</script>
 
 <style>
-.p-card-body{
+.p-card-body {
   height: 100% !important;
   display: flex !important;
   flex-direction: column !important;
-  }
+}
 
-.p-card-footer{
+.p-card-footer {
   margin-top: auto !important;
 }
 </style>
